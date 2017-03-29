61e484ef4676ce13967b54bdfad3a9c580ce53e1
Valentin Thirion <vt@abakusitsolutions.eu>
2017-03-29 16:04:40 +0200

    [ADD] hr_onboarding_outboarding Set default values

--- hr_onboarding_outboarding/models/project_project.py
+++ hr_onboarding_outboarding/models/project_project.py
@@ -7,6 +7,6 @@ _logger = logging.getLogger(__name__)
 class project_onboarding(models.Model):
     _inherit = 'project.project'
 
-    onboarding_project = fields.Boolean('Is an Onboarding Project')
-    outboarding_project = fields.Boolean('Is an Outboarding Project')
+    onboarding_project = fields.Boolean('Is an Onboarding Project', default=False)
+    outboarding_project = fields.Boolean('Is an Outboarding Project', default=False)
     on_out_boarding_employee_id = fields.Many2one('hr.employee', 'Employee')
