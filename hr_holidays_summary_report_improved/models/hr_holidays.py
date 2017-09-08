bbf794afa29500a8168c6ab56a807029ab731713
jasonpindat <j.pindat@hotmail.fr>
2017-09-08 09:22:14 +0200

    test

--- hr_holidays_summary_report_improved/models/hr_holidays.py
+++ hr_holidays_summary_report_improved/models/hr_holidays.py
@@ -4,6 +4,7 @@ from openerp import models, fields, api, exceptions, _
 
 import time
 from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 import logging
 _logger = logging.getLogger(__name__)
