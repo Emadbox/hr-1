eb792d10261321e044aa97359a14f23f02ddb11a
jasonpindat <j.pindat@hotmail.fr>
2017-09-14 10:27:25 +0200

    test

--- hr_days_off/models/hr_holidays.py
+++ hr_days_off/models/hr_holidays.py
@@ -131,8 +131,7 @@ class HrHolidays(models.Model):
             self.date_from = False
             self.number_of_days_temp = 0
 
-        self._compute_number_of_days()
-        
+        super(HrHolidays, self)._compute_number_of_days()
 
     @api.one
     @api.onchange('date_day_to', 'day_time_to')
@@ -146,4 +145,4 @@ class HrHolidays(models.Model):
             self.date_to = False
             self.number_of_days_temp = 0
 
-        self._compute_number_of_days()
+        super(HrHolidays, self)._compute_number_of_days()
--- hr_holidays_summary_report_improved/__openerp__.py
+++ hr_holidays_summary_report_improved/__openerp__.py
@@ -25,7 +25,6 @@
     'depends': ['hr'],
 
     'data': [
-        'views/hr_holidays.xml',
         'views/hr_holidays_status.xml',
         'views/hr_holidays_summary_dept.xml',
         'templates/report_hr_holidays_summary.xml'
