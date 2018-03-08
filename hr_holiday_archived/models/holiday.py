e194948b45d2f48112b489b2713e6f8090d2358d
Valentin Thirion <vt@abakusitsolutions.eu>
2018-03-08 16:29:20 +0100

    [FIX] Fixed bug of multiple holidays

--- hr_holiday_archived/models/holiday.py
+++ hr_holiday_archived/models/holiday.py
@@ -4,3 +4,11 @@ class hr_holiday_archived(models.Model):
     _inherit = 'hr.holidays'
     
     active = fields.Boolean('Active', default=True)
+
+    @api.model
+    def _archive_holiday(self):
+        for holiday in self:
+            if holiday.active:
+                holiday.active = False
+            else:
+                holiday.active = True
