fe3440d182636622289fed99b32a1ad8712bdc78
Valentin Thirion <vt@abakusitsolutions.eu>
2018-05-15 17:21:47 +0200

    [ADD] Added module to repo

---
+++ hr_recruitement_job_stages_improvement/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR Recruitement Stages Better Management",
+    'version': '9.0.1.0.0',
+    'depends': [
+        'hr_recruitment',
+    ],
+    'author': "Valentin Thirion, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Human Resources',
+    'description': """
+HR Recruitement Stages Better Management
+
+Odoo is manging the stages for job position so badly (creating for every new job a 'New' stage and always creating new ones) that I had to do this module.
+
+This module has been developed by Valentin Thirion @ AbAKUS it-solutions""",
+    'data': [
+        'views/hr_recuitment_job_view.xml',
+    ],
+}
---
+++ hr_recruitement_job_stages_improvement/models/hr_job.py
@@ -0,0 +1,15 @@
+from openerp import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class HRJob(models.Model):
+    _inherit = 'hr.job'
+
+    @api.model
+    def get_default_stages(self):
+        return self.env['hr.recruitment.stage'].search([('name', '=', "New")]).ids
+
+    stage_ids = fields.Many2many('hr.recruitment.stage', 'job_stage_rel', 'job_id', 'stage_id', 'Job Stages', default=get_default_stages)
+    
