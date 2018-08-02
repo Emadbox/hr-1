e27365ed47b5c5dd848880e7a024b0e1521f4077
Valentin Thirion <valentinthirion@gmail.com>
2018-08-02 13:05:36 +0200

    [MIG] CHanged 'official days off' to 'half-days' because official is managed in 11.0 (resources) and just need for halfday management

--- hr_holiday_half_day/models/hr_holidays.py
+++ hr_holiday_half_day/models/hr_holidays.py
@@ -6,6 +6,7 @@ from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FO
 import time
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import dateutil.parser
 import math
 import pytz
 
@@ -16,17 +17,21 @@ class HrHolidays(models.Model):
     _inherit = "hr.holidays"
     _order = 'id desc'
 
-    date_day_from = fields.Date(string="Date From")
-    date_day_to = fields.Date(string="Date To")
+    date_day_from = fields.Date(string="Date From", readonly=True,
+        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]}, track_visibility='onchange')
+    date_day_to = fields.Date(string="Date To", readonly=True,
+        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]}, track_visibility='onchange')
 
     day_time_from = fields.Selection([
         ('morning', 'Morning'),
         ('midday', 'Midday')
-    ], string="Day Time From", default='morning')
+    ], string="Day Time From", default='morning', readonly=True,
+        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]}, track_visibility='onchange')
     day_time_to = fields.Selection([
         ('midday', 'Midday'),
         ('evening', 'Evening')
-    ], string="Day Time From", default='evening')
+    ], string="Day Time From", default='evening', readonly=True,
+        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]}, track_visibility='onchange')
 
     def _check_fields(self, values):
         # Do these computation when 'removing' holidays
@@ -50,9 +55,9 @@ class HrHolidays(models.Model):
                 if date_day_from and day_time_from:
                     worktime = self.get_worktime(date_day_from, values)
                     if day_time_from == 'midday':
-                        time = worktime['midday']
+                        time = worktime['afternoon_start']
                     else:
-                        time = worktime['morning']
+                        time = worktime['morning_start']
                     #time = worktime['midday'] if day_time_from=='midday' else worktime['morning']
                     values['date_from'] = self.to_datetime(date_day_from + ' ' + self.float_time_convert(time) + ':00', self._context.get('tz')).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
                 else:
@@ -60,15 +65,15 @@ class HrHolidays(models.Model):
                 if date_day_to and day_time_to:
                     worktime = self.get_worktime(date_day_to, values)
                     if day_time_to == 'midday':
-                        time = worktime['midday']
+                        time = worktime['morning_end']
                     else:
-                        time = worktime['evening']
+                        time = worktime['afternoon_end']
                     #time = worktime['midday'] if day_time_to=='midday' else worktime['evening']
                     values['date_to'] = self.to_datetime(date_day_to + ' ' + self.float_time_convert(time) + ':00', self._context.get('tz')).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
                 else:
                     values['date_to'] = False
 
-                values['number_of_days_temp'] = self._compute_holidays_duration(values)
+            values['number_of_days_temp'] = self._get_number_of_days(values['date_from'], values['date_to'], self.employee_id.id)
 
         return values
 
@@ -104,44 +109,6 @@ class HrHolidays(models.Model):
         return timezone.localize(date).astimezone(pytz.UTC)
 
     @api.model
-    def _compute_holidays_duration(self, values = {}):
-        year_now = time.strftime('%Y')
-        if not self.env['hr.holidays.year'].search([('year', '=', year_now)]):
-            raise exceptions.ValidationError(_('The days off are not configured for this year(') + year_now + ')')
-
-        user_company = self.env.user.company_id
-
-        date_from = values.get('date_from', self.date_from)
-        date_to = values.get('date_to', self.date_to)
-        date_day_from = values.get('date_day_from', self.date_day_from)
-        date_day_to = values.get('date_day_to', self.date_day_to)
-        day_time_from = values.get('day_time_from', self.day_time_from)
-        day_time_to = values.get('day_time_to', self.day_time_to)
-
-        date_from = self.to_datetime(date_from, self._context.get('tz'))
-        date_to = self.to_datetime(date_to, self._context.get('tz'))
-
-        date_iterator = date_from
-        days_holidays_count = 0
-        while date_iterator <= date_to:
-            if not self.env['hr.holidays.period'].search([
-                '&',
-                    '|',
-                        ('company_ids', '=', False),
-                        ('company_ids', '=', user_company.id),
-                    ('date_start', '<=', date_iterator),
-                    ('date_stop', '>=', date_iterator)
-            ]):
-                days_holidays_count += 1
-                if (str(date_iterator.date()) == date_day_from and day_time_from == 'midday'):
-                    days_holidays_count -= 0.5
-                if (str(date_iterator.date()) == date_day_to and day_time_to == 'midday'):
-                    days_holidays_count -= 0.5
-
-            date_iterator += relativedelta(days=1)
-        return days_holidays_count
-
-    @api.model
     def float_time_convert(self, float_val):
         factor = float_val < 0 and -1 or 1
         val = abs(float_val)
@@ -152,38 +119,36 @@ class HrHolidays(models.Model):
 
     @api.model
     def get_worktime(self, date, values = {}):
-
-        employee_id = self.env['hr.employee'].search([('id', '=', values['employee_id'])]) if values.get('employee_id') else self.employee_id
-        calendar_ids = self.env['resource.calendar'].search([('company_id', '=', employee_id.company_id.id)])
-
+        # Generic work time
         worktime = {
-            'morning': 8.5,
-            'midday': 13.5,
-            'evening': 17.5
+            'morning_start': 8.5,
+            'morning_end': 12.5,
+            'afternoon_start': 13.5,
+            'afternoon_end': 17.5
         }
 
-        if len(calendar_ids) > 0:
-            for attendance in calendar_ids.attendance_ids:
-                if int(attendance.dayofweek) == datetime.strptime(date, DEFAULT_SERVER_DATE_FORMAT).weekday():
-                    worktime['morning'] = attendance.hour_from
-                    worktime['midday'] = (attendance.hour_to + attendance.hour_from) / 2
-                    worktime['evening'] = attendance.hour_to
-                    break
+        attendance_ids = self.employee_id.resource_id.calendar_id.attendance_ids.filtered(lambda a: int(a.dayofweek == datetime.strptime(date, DEFAULT_SERVER_DATE_FORMAT).weekday()))
+        _logger.debug("Attendances: %s", attendance_ids)
+        if len(attendance_ids) == 2:
+            worktime['morning_start'] = attendance_ids[0].hour_from if (attendance_ids[0].hour_from <= worktime['morning_start']) else worktime['morning_start']
+            worktime['morning_end'] = attendance_ids[0].hour_to if (attendance_ids[0].hour_to >= worktime['morning_end']) else worktime['morning_end']
+            worktime['afternoon_start'] = attendance_ids[1].hour_from if (attendance_ids[1].hour_from <= worktime['afternoon_start']) else worktime['afternoon_start']
+            worktime['afternoon_end'] = attendance_ids[1].hour_to if (attendance_ids[1].hour_to >= worktime['afternoon_end']) else worktime['afternoon_end']
+        elif len(attendance_ids) == 1:
+            worktime['morning_start'] = attendance_ids[0].hour_from if (attendance_ids[0].hour_from <= worktime['morning_start']) else worktime['morning_start']
+            worktime['afternoon_end'] = attendance_ids[0].hour_to if (attendance_ids[0].hour_to >= worktime['afternoon_end']) else worktime['afternoon_end']
 
         return worktime
 
-    def _get_number_of_days(self, date_from, date_to, employee_id):
-        return self._compute_holidays_duration()
-
+    @api.multi
     @api.onchange('date_day_from', 'day_time_from', 'date_day_to', 'day_time_to')
     def _onchange_dates(self):
-        if self.date_day_from and self.date_day_to:
-            worktime = self.get_worktime(self.date_day_from)
-            time_from = worktime['midday'] if self.day_time_from == 'midday' else worktime['morning']
-            time_to = worktime['midday'] if self.day_time_to == 'midday' else worktime['evening']
+        for leave in self:
+            if leave.date_day_from and leave.date_day_to:
+                worktime = leave.get_worktime(leave.date_day_from)
+                time_from = worktime['afternoon_start'] if leave.day_time_from == 'midday' else worktime['morning_start']
+                time_to = worktime['afternoon_start'] if leave.day_time_to == 'midday' else worktime['afternoon_end']
 
-            self.date_from = self.to_datetime(self.date_day_from + ' ' + self.float_time_convert(time_from) + ':00', self._context.get('tz'))
-            self.date_to = self.to_datetime(self.date_day_to + ' ' + self.float_time_convert(time_to) + ':00', self._context.get('tz'))
-
-            count = self._compute_holidays_duration()
-            self.number_of_days_temp = count
+                leave.date_from = self.to_datetime(leave.date_day_from + ' ' + self.float_time_convert(time_from) + ':00', self._context.get('tz'))
+                leave.date_to = self.to_datetime(leave.date_day_to + ' ' + self.float_time_convert(time_to) + ':00', self._context.get('tz'))
+                leave.number_of_days_temp = self._get_number_of_days(leave.date_from, leave.date_to, self.employee_id.id)
--- hr_holidays_calendar_improvements/README.md
+++
@@ -1,5 +0,0 @@
-#HR Leaves calendar improvements
-
-Shows the leave states in the calendar view.
-
-This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions.
