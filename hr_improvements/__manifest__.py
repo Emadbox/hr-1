f9c5d350de1c23bab864063fc375513cfbafd79b
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-28 10:25:55 +0200

    [MIG] HR Abakus Improvements

---
+++ hr_improvements/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
+{
+    "name": "HR AbAKUS Improvements",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Human resources',
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "views/hr_view.xml",
+    ],
+    'installable': True
+}
--- hr_improvements/models/hr.py
+++ hr_improvements/models/hr.py
@@ -1,16 +1,18 @@
-from openerp import models, fields
-from datetime import timedelta
+# -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
+from odoo import models, fields
 
-class hr_employee(models.Model):
+
+class HrEmployee(models.Model):
     _inherit = ['hr.employee']
 
     # Add a new columns
     entry_date = fields.Date('Start date')
-    
-    key_number = fields.Char('Key number')
+
+    key_number = fields.Char()
     has_badge = fields.Boolean('Has a building badge')
 
-    computer_model = fields.Char('Computer model')
+    computer_model = fields.Char()
     computer_serial = fields.Char('Computer serial number')
     computer_acquisition_date = fields.Date('Date of acquisition')
 
@@ -18,6 +20,6 @@ class hr_employee(models.Model):
                                  string='Fuel company',
                                  help='Select a partner for this fuel card if it exists.',
                                  ondelete='restrict')
-    fuel_card_number = fields.Char('Fuel card number')
-    fuel_card_pin = fields.Char('Fuel card PIN')
-    notes = fields.Char('Notes')
+    fuel_card_number = fields.Char()
+    fuel_card_pin = fields.Char()
+    notes = fields.Char()
