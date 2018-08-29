2b7f4175eae37b9a7206fed6bc510ad8a47ef1c1
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-29 11:15:35 +0200

    [MIG] Print Timesheet Report

---
+++ hr_timesheet_report/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
+{
+    'name': "Timesheet Report Printing",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': ['hr'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'HR',
+    'summary': "Timesheet as a beautiful and signable report.",
+    'data': [
+        'report/hr_timesheet_report.xml',
+    ],
+}
