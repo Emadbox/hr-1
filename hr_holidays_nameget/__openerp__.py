4cda20e55e92e4c48a08034116a9c80634201f44
fwyaime <f.wyaime@gmail.com>
2019-02-13 16:52:30 +0100

    [ADD] nameget changed on holidays

---
+++ hr_holidays_nameget/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Hr Holidays Name Get",
+    'version': '9.0.1.0',
+    'depends': [
+        'hr_holidays',
+    ],
+    'author': "François Wyaime @ AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Hr',
+    'description': """
+Hr Holidays Name Get
+
+This module add the date of leaves defined by employees to nameget.
+
+This module has been developed by AbAKUS it-solutions""",
+    'data': [
+    ]
+}
